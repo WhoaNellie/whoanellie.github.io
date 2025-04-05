c750f6774d187f500b0f5057a2895fd099738e03
nellie <NaN@nellienelson.com>
2025-04-05 13:27:20 -0700

    add construction icons

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,241 +1,231 @@
 import React from 'react';
+import ConstructionPattern from "../components/ConstructionPattern";
 
-function Projects(){
-    return (
-      <main>
-        <svg className="halfHex rTwo left mobileHide" viewBox="0 0 100 43">
-          <polyline points="0 0, 25 43, 75 43, 100 0" fill="#80beca"></polyline>
-        </svg>
+function Projects() {
+  return (
+    <main>
+      <svg className="halfHex rTwo left mobileHide" viewBox="0 0 100 43">
+        <polyline points="0 0, 25 43, 75 43, 100 0" fill="#80beca"></polyline>
+      </svg>
 
-        <svg className="halfHex rTwo right mobileHide" viewBox="0 0 100 43">
-          <polyline points="0 0, 25 43, 75 43, 100 0" fill="#80beca"></polyline>
-        </svg>
+      <svg className="halfHex rTwo right mobileHide" viewBox="0 0 100 43">
+        <polyline points="0 0, 25 43, 75 43, 100 0" fill="#80beca"></polyline>
+      </svg>
 
-        <svg className="fullHex center" viewBox="0 0 100 86">
-          <g className="projHex">
-            <polygon
-              className="mobileHide titleHex"
-              points="0,43 25,0 75,86 25,86"
-              fill="#80beca"
-            ></polygon>
+      <svg className="fullHex center" viewBox="0 0 100 86">
+        <ConstructionPattern />
+        <g className="projHex">
+          <polygon
+            className="mobileHide titleHex"
+            points="0,43 25,0 75,86 25,86"
+            fill="#80beca"
+          ></polygon>
 
+          <polygon
+            className="shape"
+            points="25 0, 50 43, 75 0"
+            strokeWidth="0"
+            fill="#fbf8e5"
+          ></polygon>
+          <image width="20" x="40" y="6" href="assets/images/rings.svg"></image>
+          <text
+            x="39"
+            y="-6"
+            transform="rotate(60)"
+            className="projText"
+            id="code"
+          >
+            RINGS
+          </text>
+          <text
+            x="42"
+            y="8"
+            transform="rotate(60)"
+            className="projText"
+            id="quiz"
+          >
+            TRUE
+          </text>
+
+          <a className="triLink" href="https://github.com/WhoaNellie/ringsTrue">
             <polygon
               className="shape"
-              points="25 0, 50 43, 75 0"
+              points="75 0, 50 43, 100 43"
+              strokeWidth="0"
+              fill="#4ba2b2"
+            ></polygon>
+            <text
+              x="36.5"
+              y="12"
+              transform="rotate(60, 50, 43)"
+              className="gitlink repo"
+            >
+              REPO
+            </text>
+          </a>
+          <a className="triLink" href="https://nellienelson.com/ringsTrue/">
+            <polygon
+              className="shape underConstruction"
+              points="50 43, 75 86, 100 43"
+              strokeWidth="0"
+              fill="url(#constructionIcon)"
+            ></polygon>
+            <text
+              x="60"
+              y="40"
+              transform="rotate(60, 50, 43)"
+              className="gitlink page"
+            >
+              PAGE
+            </text>
+          </a>
+        </g>
+      </svg>
+
+      <svg className="fullHex left" viewBox="0 0 100 86">
+        <g className="projHex">
+          <polygon
+            className="mobileHide titleHex"
+            points="0,43 25,0 75,86 25,86"
+            fill="#80beca"
+          ></polygon>
+          <text
+            x="41"
+            y="-7"
+            transform="rotate(60)"
+            className="projText"
+            id="password"
+          >
+            CUT-UP
+          </text>
+          <text
+            x="37"
+            y="7"
+            transform="rotate(60)"
+            className="projText"
+            id="generator"
+          >
+            POEMS
+          </text>
+          <polygon
+            className="shape"
+            points="25 0, 50 43, 75 0"
+            strokeWidth="0"
+            fill="#fbf8e5"
+          ></polygon>
+          <image
+            width="18"
+            x="41"
+            y="6"
+            href="assets/images/scissors.svg"
+          ></image>
+
+          <a
+            className="triLink"
+            href="https://github.com/WhoaNellie/Cut-Up-Poems"
+          >
+            <polygon
+              className="shape"
+              points="75 0, 50 43, 100 43"
+              strokeWidth="0"
+              fill="#4ba2b2"
+            ></polygon>
+            <text
+              x="36.5"
+              y="12"
+              transform="rotate(60, 50, 43)"
+              className="gitlink repo"
+            >
+              REPO
+            </text>
+          </a>
+
+          <a className="triLink" href="https://nellienelson.com/Cut-Up-Poems/">
+            <polygon
+              className="shape"
+              points="50 43, 75 86, 100 43"
               strokeWidth="0"
               fill="#fbf8e5"
             ></polygon>
-            <image
-              width="20"
-              x="40"
-              y="6"
-              href="assets/images/rings.svg"
-            ></image>
             <text
-              x="39"
-              y="-6"
-              transform="rotate(60)"
-              className="projText"
-              id="code"
+              x="60"
+              y="40"
+              transform="rotate(60, 50, 43)"
+              className="gitlink page"
             >
-              RINGS
+              PAGE
             </text>
-            <text
-              x="42"
-              y="8"
-              transform="rotate(60)"
-              className="projText"
-              id="quiz"
-            >
-              TRUE
-            </text>
+          </a>
+        </g>
+      </svg>
 
-            <a
-              className="triLink"
-              href="https://github.com/WhoaNellie/ringsTrue"
-            >
-              <polygon
-                className="shape"
-                points="75 0, 50 43, 100 43"
-                strokeWidth="0"
-                fill="#4ba2b2"
-              ></polygon>
-              <text
-                x="36.5"
-                y="12"
-                transform="rotate(60, 50, 43)"
-                className="gitlink repo"
-              >
-                REPO
-              </text>
-            </a>
+      <svg className="fullHex right" viewBox="0 0 100 86">
+        <g className="projHex">
+          <polygon
+            className="mobileHide titleHex"
+            points="0,43 25,0 75,86 25,86"
+            fill="#80beca"
+          ></polygon>
+          <text
+            x="30"
+            y="-3"
+            transform="rotate(60)"
+            className="projText"
+            id="weather"
+          >
+            CALENDY
+          </text>
 
-            <a className="triLink" href="https://nellienelson.com/ringsTrue/">
-              <polygon
-                className="shape"
-                points="50 43, 75 86, 100 43"
-                strokeWidth="0"
-                fill="#fbf8e5"
-              ></polygon>
-              <text
-                x="60"
-                y="40"
-                transform="rotate(60, 50, 43)"
-                className="gitlink page"
-              >
-                PAGE
-              </text>
-            </a>
-          </g>
-        </svg>
+          <polygon
+            className="shape"
+            points="25 0, 50 43, 75 0"
+            strokeWidth="0"
+            fill="#fbf8e5"
+          ></polygon>
+          <image
+            width="20"
+            x="40"
+            y="6"
+            href="assets/images/calendy.svg"
+          ></image>
 
-        <svg className="fullHex left" viewBox="0 0 100 86">
-          <g className="projHex">
-            <polygon
-              className="mobileHide titleHex"
-              points="0,43 25,0 75,86 25,86"
-              fill="#80beca"
-            ></polygon>
-            <text
-              x="41"
-              y="-7"
-              transform="rotate(60)"
-              className="projText"
-              id="password"
-            >
-              CUT-UP
-            </text>
-            <text
-              x="37"
-              y="7"
-              transform="rotate(60)"
-              className="projText"
-              id="generator"
-            >
-              POEMS
-            </text>
+          <a className="triLink" href="https://github.com/WhoaNellie/calendy">
             <polygon
               className="shape"
-              points="25 0, 50 43, 75 0"
+              points="75 0, 50 43, 100 43"
               strokeWidth="0"
-              fill="#fbf8e5"
-            ></polygon>
-            <image
-              width="18"
-              x="41"
-              y="6"
-              href="assets/images/scissors.svg"
-            ></image>
-
-            <a
-              className="triLink"
-              href="https://github.com/WhoaNellie/Cut-Up-Poems"
-            >
-              <polygon
-                className="shape"
-                points="75 0, 50 43, 100 43"
-                strokeWidth="0"
-                fill="#4ba2b2"
-              ></polygon>
-              <text
-                x="36.5"
-                y="12"
-                transform="rotate(60, 50, 43)"
-                className="gitlink repo"
-              >
-                REPO
-              </text>
-            </a>
-
-            <a
-              className="triLink"
-              href="https://nellienelson.com/Cut-Up-Poems/"
-            >
-              <polygon
-                className="shape"
-                points="50 43, 75 86, 100 43"
-                strokeWidth="0"
-                fill="#fbf8e5"
-              ></polygon>
-              <text
-                x="60"
-                y="40"
-                transform="rotate(60, 50, 43)"
-                className="gitlink page"
-              >
-                PAGE
-              </text>
-            </a>
-          </g>
-        </svg>
-
-        <svg className="fullHex right" viewBox="0 0 100 86">
-          <g className="projHex">
-            <polygon
-              className="mobileHide titleHex"
-              points="0,43 25,0 75,86 25,86"
-              fill="#80beca"
+              fill="#4ba2b2"
             ></polygon>
             <text
-              x="30"
-              y="-3"
-              transform="rotate(60)"
-              className="projText"
-              id="weather"
+              x="36.5"
+              y="12"
+              transform="rotate(60, 50, 43)"
+              className="gitlink repo"
             >
-              CALENDY
+              REPO
             </text>
+          </a>
 
+          <a className="triLink" href="http://calendy.nellienelson.com/">
             <polygon
-              className="shape"
-              points="25 0, 50 43, 75 0"
+              className="shape underConstruction"
+              points="50 43, 75 86, 100 43"
               strokeWidth="0"
-              fill="#fbf8e5"
+              fill="url(#constructionIcon)"
             ></polygon>
-            <image
-              width="20"
-              x="40"
-              y="6"
-              href="assets/images/calendy.svg"
-            ></image>
-
-            <a className="triLink" href="https://github.com/WhoaNellie/calendy">
-              <polygon
-                className="shape"
-                points="75 0, 50 43, 100 43"
-                strokeWidth="0"
-                fill="#4ba2b2"
-              ></polygon>
-              <text
-                x="36.5"
-                y="12"
-                transform="rotate(60, 50, 43)"
-                className="gitlink repo"
-              >
-                REPO
-              </text>
-            </a>
-
-            <a className="triLink" href="http://calendy.nellienelson.com/">
-              <polygon
-                className="shape"
-                points="50 43, 75 86, 100 43"
-                strokeWidth="0"
-                fill="#fbf8e5"
-              ></polygon>
-              <text
-                x="60"
-                y="40"
-                transform="rotate(60, 50, 43)"
-                className="gitlink page"
-              >
-                PAGE
-              </text>
-            </a>
-          </g>
-        </svg>
-      </main>
-    );
+            <text
+              x="60"
+              y="40"
+              transform="rotate(60, 50, 43)"
+              className="gitlink page"
+            >
+              PAGE
+            </text>
+          </a>
+        </g>
+      </svg>
+    </main>
+  );
 }
 
 export default Projects;
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,17 @@
-let path = require("path");
+const path = require("path");
+
 module.exports = {
+  entry: "./src/index.jsx",
+  output: {
+    path: path.resolve(__dirname, "./dist/js"),
+    filename: "script.bundle.js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
   module: {
     rules: [
+      // JS/JSX loader
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
@@ -9,25 +19,38 @@ module.exports = {
           loader: "babel-loader",
         },
       },
+
+      // SASS loader
       {
         test: /\.s[ac]ss$/i,
         use: [
-          // Creates `style` nodes from JS strings
-          "style-loader",
-          // Translates CSS into CommonJS
-          "css-loader",
-          // Compiles Sass to CSS
-          "sass-loader",
+          "style-loader", // Injects styles into DOM
+          "css-loader", // Turns CSS into CommonJS
+          "sass-loader", // Compiles Sass to CSS
+        ],
+      },
+
+      // SVG loader for React component imports
+      {
+        test: /\.svg$/,
+        issuer: /\.[jt]sx?$/,
+        use: ["@svgr/webpack"],
+      },
+
+      // Asset loader fallback for images/fonts
+      {
+        test: /\.(png|jpe?g|gif|svg|woff2?|eot|ttf|otf)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[path][name].[ext]",
+            },
+          },
         ],
       },
     ],
   },
-  entry: "./src/index.jsx",
-  output: {
-    path: path.resolve("./dist/js"),
-    filename: "script.bundle.js",
-  },
-  resolve: { extensions: [".js", ".jsx"] },
   mode: "development",
   watch: true,
 };
